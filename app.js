560347189394879f86f7166573006e3a4729ee84
Mirae-16 <mariamatef1122008@gmail.com>
2026-07-01 22:03:38 +0300

    SecondAdd

--- app.js
+++ app.js
@@ -3,3 +3,26 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const app = express();
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const app = express();
+
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>console.log("MongoDB Connected"))
+.catch(err=>console.log(err));
+
+// app.use("/api/auth", require("./routes/authRoutes"));
+// app.use("/api/products", require("./routes/productRoutes"));
+// app.use("/api/categories", require("./routes/categoryRoutes"));
+// app.use("/api/cart", require("./routes/cartRoutes"));
+// app.use("/api/orders", require("./routes/orderRoutes"));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT,()=>{
+    console.log(`Server Running on ${PORT}`);
+});
