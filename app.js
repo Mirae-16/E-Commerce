849d6ac4e35902b2f935f6f177e69f35e04f6208
Mirae-16 <mariamatef1122008@gmail.com>
2026-07-01 22:22:41 +0300

    MuchSimpler

--- app.js
+++ app.js
@@ -15,11 +15,7 @@ mongoose.connect(process.env.MONGO_URI)
 .then(()=>console.log("MongoDB Connected"))
 .catch(err=>console.log(err));
 
-// app.use("/api/auth", require("./routes/authRoutes"));
-// app.use("/api/products", require("./routes/productRoutes"));
-// app.use("/api/categories", require("./routes/categoryRoutes"));
-// app.use("/api/cart", require("./routes/cartRoutes"));
-// app.use("/api/orders", require("./routes/orderRoutes"));
+app.use("/api/auth", require("./routes/ProductsRouter.js"))
 
 const PORT = process.env.PORT || 5000;
 
